53517ef76b665d953107c7e3ce0408e42560d355
Afteryou1319 <it-afteryou@afteryou.co.th>
2024-12-05 21:31:24 +0700

    add Project sortedlist

---
+++ SortedList_homework/SortedList_homework/SortedList_homework/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortedList_homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedlistl;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedlistl = new SortedList();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            sortedlistl.Add(int.Parse(keytxt.Text) , valuetxt.Text);
+            ICollection collection = sortedlistl.Keys;
+            string allItems = "Current SortedList items:\n";
+            foreach (var a in sortedlistl.Keys)
+            {
+                allItems += $"Key: {a}, Value: {sortedlistl[a]}\n";
+            }
+            MessageBox.Show(allItems, "SortedList Items", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
